de73440ed8463c8e195383609e7acf4d81a9c278
paulgibes <paulgibes@gmail.com>
2023-02-16 18:35:38 -0700

    created a high scores 'page'

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,33 @@
 // Element.innerHTML = ;
 // Element.SetHTML();
 // Node.textContent
+var startBtn = document.querySelector(".startBtn");
+var scoresBtn = document.querySelector(".scoresBtn");
+var questionNumber = document.querySelector("#questionNumber");
+var questionEl = document.querySelector(".questionEl");
 var timeEl = document.querySelector(".seconds");
 var time = Number(timeEl.textContent);
 var timeInterval;
+var o1 = document.querySelector("#o1");
+var o2 = document.querySelector("#o2");
+var o3 = document.querySelector("#o3");
+var o4 = document.querySelector("#o4");
 var index = 0;
+var defANewPage = 0;
 var questions = [
   {
     Q: "Question 1",
-    O: ["option1", "Option2", "Option3", "Option4"],
+    O: ["Option1", "Option2", "Option3", "Option4"],
     A: "Option1",
   },
   {
     Q: "Question 2",
-    O: ["option1", "Option2", "Option3", "Option4"],
+    O: ["Option1", "Option2", "Option3", "Option4"],
     A: "Option3",
   },
   {
     Q: "Question 3",
-    O: ["option1", "Option2", "Option3", "Option4"],
+    O: ["Option1", "Option2", "Option3", "Option4"],
     A: "Option4",
   },
 ];
@@ -32,7 +41,7 @@ function start() {
     }
   }, 1000);
 
-  document.querySelector(".scoresBtn").style = "visibility:hidden";
+  scoresBtn.style = "visibility:hidden";
   document.querySelector("#startSection").style = "display:none";
   document.querySelector("#questionSection").style = "display:block";
 
@@ -41,11 +50,13 @@ function start() {
 
 function nextQuestion() {
   if (index < questions.length) {
-    document.querySelector(".questionEl").textContent = questions[index].Q;
-    document.querySelector("#o1").textContent = questions[index].O[0];
-    document.querySelector("#o2").textContent = questions[index].O[1];
-    document.querySelector("#o3").textContent = questions[index].O[2];
-    document.querySelector("#o4").textContent = questions[index].O[3];
+    questionNumber.textContent = 1 + index;
+    questionEl.textContent = questions[index].Q;
+    o1.textContent = questions[index].O[0];
+    o2.textContent = questions[index].O[1];
+    o3.textContent = questions[index].O[2];
+    o4.textContent = questions[index].O[3];
+  } else {
   }
   index++;
 }
@@ -54,4 +65,17 @@ function endGame() {
   clearInterval(timeInterval);
 }
 
-document.querySelector(".startBtn").addEventListener("click", start);
+function showScores() {
+  if (defANewPage === 0) {
+    document.querySelector("#startSection").style = "display:none";
+    document.querySelector("#scoresSection").style = "display:block";
+    defANewPage++;
+  } else {
+    document.querySelector("#startSection").style = "display:block";
+    document.querySelector("#scoresSection").style = "display:none";
+    defANewPage--;
+  }
+}
+
+startBtn.addEventListener("click", start);
+scoresBtn.addEventListener("click", showScores);
